0d06a0a24e69b929f7ef9a33a7d383de8260680f
Radha priyadarsan <radhapriyan786@gmail.com>
2024-05-31 15:24:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+
+const app = express();
+const PORT = 3000;
+const folderPath = path.join(__dirname, 'files');
+
+// Ensure the folder exists
+if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+}
+
+// Endpoint to create a text file with the current timestamp
+app.post('/create-timestamp', (req, res) => {
+    const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+    const filename = moment().format('YYYY-MM-DD_HH-mm-ss') + '.txt';
+    const filePath = path.join(folderPath, filename);
+
+    fs.writeFile(filePath, timestamp, (err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error writing file', error: err });
+        }
+        res.status(200).json({ message: 'File created', filename });
+    });
+});
+
+// Endpoint to retrieve all text files in the folder
+app.get('/list-timestamps', (req, res) => {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading directory', error: err });
+        }
+        res.status(200).json({ files });
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

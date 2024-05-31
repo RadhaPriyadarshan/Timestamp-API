dd26659d015008ca60960f467b440b1fb4eb3b5a
Radha priyadarsan <radhapriyan786@gmail.com>
2024-05-31 16:03:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const moment = require('moment');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const folderPath = path.join(__dirname, 'files');
 
 
